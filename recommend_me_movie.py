8342d89a8bb9088c5990d53ef180e77aacad6405
Roman Volodin <roman.volodin@gmail.com>
2021-08-07 21:17:56 +0300

    Use argparse to read a movie title

--- recommend_me_movie.py
+++ recommend_me_movie.py
@@ -1,3 +1,5 @@
+import argparse
+
 from environs import Env
 import requests
 
@@ -5,14 +7,27 @@ import requests
 TMDB_API_URL = "https://api.themoviedb.org/3"
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'movie_title',
+        help = 'Movie title for recommendations',
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
     env = Env()
     env.read_env() 
 
     api_key = env.str("RMM_API_KEY")
+
+    args = parse_arguments()
+
     payload = {
         "api_key": api_key,
         "language": "ru-RU",
-        "query": "Terminator",
+        "query": args.movie_title,
     }
     response = requests.get(f"{TMDB_API_URL}/search/movie", params=payload, timeout=10)
+    print(response.json())
